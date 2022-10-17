6594b471c9a8fdf5a92a7670f73edba868ac9c24
Fatema04 <shadrinmoni15@gmail.com>
2022-10-16 20:00:15 -0700

    input pass  added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,31 @@ import { getAuth } from "firebase/auth";
 import app from "./firebase/firebase.init";
 import './App.css';
 const auth = getAuth(app);
+
+// target full form
+const handleSubmit = (event) => {
+  const email = event.target.email.value
+  const password = event.target.password.value
+  event.preventDefault()
+  console.log(email, password)
+}
+
+// target email form
+const handleEmailOnBlur = event => {
+  console.log(event.target.value)
+}
+// target password form
+const handlePasswordOnBlur = event => {
+  console.log(event.target.value)
+}
+
 function App() {
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e)
-  }
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
-        <input type='email' name="email" placeholder="Email" />
+        <input onBlur={handleEmailOnBlur} type='email' name="email" placeholder="Email" />
         <br></br>
-        <input type='password' name="password" placeholder="password" />
+        <input onBlur={handlePasswordOnBlur} type='password' name="password" placeholder="password" />
         <br></br>
         <button type="submit">Register</button>
       </form>
