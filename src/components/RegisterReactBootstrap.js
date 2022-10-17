3029ddbf185c9914e7f5601a1142bce50bb750bb
Fatema04 <shadrinmoni15@gmail.com>
2022-10-16 22:01:45 -0700

    input pass validation added

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -3,15 +3,36 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import app from '../firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 const RegisterReactBootstrap = () => {
 
+    // custum password step1
+
+    const [passError, setPassError] = useState('')
+
     // target full form
     const handleSubmit = (event) => {
         const email = event.target.email.value
         const password = event.target.password.value
+
+        // custum password step2
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setPassError('Please enter two capital letter')
+            return;
+        }
+        if (password.length < 6) {
+            setPassError('Password should be at least 6 characters')
+        }
+        if (!/(?=.*[@#$%&!])/.test(password)) {
+            setPassError('Please enter at least one special character')
+            return;
+        }
+        setPassError('')
+
+
         event.preventDefault()
         //console.log(email, password)
         createUserWithEmailAndPassword(auth, email, password)
@@ -40,6 +61,7 @@ const RegisterReactBootstrap = () => {
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
+                <p className='text-danger'>{passError}</p>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
