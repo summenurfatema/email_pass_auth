e4eb915a3679a5d895cb9306ba2bc1cd64ef5336
Fatema04 <shadrinmoni15@gmail.com>
2022-10-16 21:08:21 -0700

    input pass handler  added

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,6 +1,10 @@
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.init';
+
+const auth = getAuth(app)
 
 const RegisterReactBootstrap = () => {
 
@@ -9,7 +13,18 @@ const RegisterReactBootstrap = () => {
         const email = event.target.email.value
         const password = event.target.password.value
         event.preventDefault()
-        console.log(email, password)
+        //console.log(email, password)
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                // Signed in 
+                const user = result.user;
+                console.log(user)
+
+            })
+            .catch((error) => {
+
+                console.log('error', error)
+            });
     }
 
     return (
